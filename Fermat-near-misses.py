f547d5faa0415d89ea05a214a318f32fbadee842
Collins Bett <collinsbett@Collinss-MacBook-Pro.local>
2022-01-20 11:58:05 +0300

    Square a third number

--- Fermat-near-misses.py
+++ Fermat-near-misses.py
@@ -3,6 +3,8 @@ import math
 def nearMiss(n):
     ans = 0
 
-        for i in range(1, n + 1):
-        for j in range(i, n + 1):
-            x = i * i + j * j
+    for i in range(1, n + 1):
+     for j in range(i, n + 1):
+        x = i * i + j * j
+
+        y = int(math.sqrt(x))
